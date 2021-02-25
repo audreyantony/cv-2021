2b4644c0a2eeb1764da3c2977da40700786b3c88
audreyantony <web2020.audrey@gmail.com>
2021-02-25 09:11:06 +0100

    changes in the text (home.page)

--- view/user/home.user.view.php
+++ view/user/home.user.view.php
@@ -3,6 +3,6 @@
         <!-- TITLE -->
         <h1>{ Hello World }</h1>
         <!-- SMALL ABOUT -->
-        <p><img src="img/picture/cerf.png" alt="small fawn" title="Picture of a fawn">Hi there ! My name is Audrey and I'm training to be a web developer ! Early in 2020 this portfolio was a school project, but since I've adapted it to become a real presentation website with a resume/about me section, and to display my projects, on behalf of the school or on the side. I'm currently looking for a internship/traineeship between the beginning of may and the mid-June, 6 weeks totals. You can reach me by sending an e-mail via the contact page; I'll be happy to answer any question !</p>
+        <p><img src="img/picture/cerf.png" alt="small fawn" title="Picture of a fawn">Hi there !<br><br> My name is Audrey and I'm a web developer in training ! Early in 2020 this portfolio was a school project, but since I've adapted it to become a real presentation website with a resume/about me section, and to display my projects, on behalf of the school or on the side. Like a newborn fawn, my skills are newly acquired and I'm still learning how to run, but I'm working toward it ! I'm currently looking for a internship/traineeship between the beginning of may and the mid-June, 6 weeks totals.<br><br> You can reach me by sending an e-mail via the contact page; I'll be happy to answer any question !</p>
     </div>
 </section>
