f510ba1aee732799560190f7b6aaafeeb96fbc3e
audreyantony <web2020.audrey@gmail.com>
2021-03-01 20:08:02 +0100

    start

--- view/user/about.user.view.php
+++ view/user/about.user.view.php
@@ -25,34 +25,63 @@
             <div class="slide two">
                 <h3>Education</h3>
                 <ul>
-                    <li><h6>TRUC</h6><p>INFO SUPP</p></li>
-                    <li><h6>TRUC</h6><p>INFO SUPP</p></li>
-                    <li><h6>TRUC</h6><p>INFO SUPP</p></li>
-                    <li><h6>TRUC</h6><p>INFO SUPP</p></li>
+                    <li><h6>Web Developer</h6><p>CF2m training center <br> 01/2020 - 04/2021</p></li>
+                    <li><h6>Interior Design</h6><p>Lignes et formations <br> 09/2010 - 09/2013</p></li>
+                    <li><h6>Management science</h6><p>Carcado-Saisseval high school <br> -> 2010</p></li>
                 </ul>
             </div>
             <div class="slide three">
                 <h3>Skills</h3>
                 <ul>
-                    <li><h6>SKILL</h6><p>INFO SUPP</p></li>
-                    <li><h6>SKILL</h6><p>INFO SUPP</p></li>
-                    <li><h6>SKILL</h6><p>INFO SUPP</p></li>
-                    <li><h6>SKILL</h6><p>INFO SUPP</p></li>
-                    <li><h6>SKILL</h6><p>INFO SUPP</p></li>
-                    <li><h6>SKILL</h6><p>INFO SUPP</p></li>
-                    <li><h6>SKILL</h6><p>INFO SUPP</p></li>
-                    <li><h6>SKILL</h6><p>INFO SUPP</p></li>
-                    <li><h6>SKILL</h6><p>INFO SUPP</p></li>
-                    <li><h6>SKILL</h6><p>INFO SUPP</p></li>
+                    <li><h6>HTML - CSS</h6><p><i class="fa fa-circle" aria-hidden="true"></i>
+                            <i class="fa fa-circle" aria-hidden="true"></i>
+                            <i class="fa fa-circle" aria-hidden="true"></i>
+                        <br>good knowledge</p></li>
+                    <li><h6>PHP</h6><p><i class="fa fa-circle" aria-hidden="true"></i>
+                            <i class="fa fa-circle" aria-hidden="true"></i>
+                            <i class="fa fa-circle-thin" aria-hidden="true"></i><br>
+                            intermediate knowledge</p></li>
+                    <li><h6>JavaScript</h6><p><i class="fa fa-circle" aria-hidden="true"></i>
+                            <i class="fa fa-circle" aria-hidden="true"></i>
+                            <i class="fa fa-circle-thin" aria-hidden="true"></i><br>
+                            intermediate knowledge</p></li>
+                    <li><h6>Python</h6><p><i class="fa fa-circle" aria-hidden="true"></i>
+                            <i class="fa fa-circle-thin" aria-hidden="true"></i>
+                            <i class="fa fa-circle-thin" aria-hidden="true"></i><br>
+                            basic knowledge</p></li>
+                    <li><h6>CMS</h6><p><i class="fa fa-circle" aria-hidden="true"></i>
+                            <i class="fa fa-circle" aria-hidden="true"></i>
+                            <i class="fa fa-circle-thin" aria-hidden="true"></i><br>
+                            intermediate knowledge</p></li>
+                    <li><h6>Bootstrap</h6><p><i class="fa fa-circle" aria-hidden="true"></i>
+                            <i class="fa fa-circle" aria-hidden="true"></i>
+                            <i class="fa fa-circle-thin" aria-hidden="true"></i><br>
+                            intermediate knowledge</p></li>
+                    <li><h6>SQL</h6><p><i class="fa fa-circle" aria-hidden="true"></i>
+                            <i class="fa fa-circle" aria-hidden="true"></i>
+                            <i class="fa fa-circle-thin" aria-hidden="true"></i><br>
+                            intermediate knowledge</p></li>
+                    <li><h6>Workbench</h6><p><i class="fa fa-circle" aria-hidden="true"></i>
+                            <i class="fa fa-circle" aria-hidden="true"></i>
+                            <i class="fa fa-circle-thin" aria-hidden="true"></i><br>
+                            intermediate knowledge</p></li>
+                    <li><h6>Git</h6><p><i class="fa fa-circle" aria-hidden="true"></i>
+                            <i class="fa fa-circle" aria-hidden="true"></i>
+                            <i class="fa fa-circle-thin" aria-hidden="true"></i><br>
+                            intermediate knowledge</p></li>
+                    <li><h6>Photoshop</h6><p><i class="fa fa-circle" aria-hidden="true"></i>
+                        <i class="fa fa-circle" aria-hidden="true"></i>
+                        <i class="fa fa-circle-thin" aria-hidden="true"></i><br>
+                        intermediate knowledge</p></li>
                 </ul>
             </div>
             <div id="scroll-right" class="slide four">
                 <h3>WORK EXPRERIENCE</h3>
                 <ul>
-                    <li><h6>TRUC <span>YEAR - YEAR</span></h6><p>INFO SUPP</p></li>
-                    <li><h6>TRUC <span>YEAR - YEAR</span></h6><p>INFO SUPP</p></li>
-                    <li><h6>TRUC <span>YEAR - YEAR</span></h6><p>INFO SUPP</p></li>
-                    <li><h6>TRUC <span>YEAR - YEAR</span></h6><p>INFO SUPP</p></li>
+                    <li><h6>Intermittent manager<br><span>03/2019 - 11/2019</span></h6><p>Overstock, Braine l'Alleud</p></li>
+                    <li><h6>Commercial<br><span>05/2014 - 03/2019</span></h6><p>Overstock, Braine l'Alleud</p></li>
+                    <li><h6>Head waitress<br><span>11/2011 - 09/2013</span></h6><p>Saï restaurant, Paris</p></li>
+                    <li><h6>Waitress - Head waitress<br><span>08/2011 - 11/2011</span></h6><p>Interim in Paris</p></li>
                 </ul>
             </div>
         </div>
@@ -61,10 +90,10 @@
         <div id="languages">
             <h3>Languages</h3>
             <ul>
-                <li><h6>Langue</h6><p>INFO SUPP</p></li>
-                <li><h6>Langue</h6><p>INFO SUPP</p></li>
-                <li><h6>Langue</h6><p>INFO SUPP</p></li>
-                <li><h6>Langue</h6><p>INFO SUPP</p></li>
+                <li><h6>French</h6><p>Mother tongue</p></li>
+                <li><h6>English</h6><p>Good oral/written level</p></li>
+                <li><h6>Dutch</h6><p>Understood language</p></li>
+                <li><h6>German</h6><p>Understood language</p></li>
             </ul>
         </div>
         <div id="main-about-img">
@@ -73,10 +102,10 @@
         <div id="interest">
             <h3>Interest</h3>
             <ul>
-                <li><h6>hobbies</h6><p>INFO SUPP</p></li>
-                <li><h6>hobbies</h6><p>INFO SUPP</p></li>
-                <li><h6>hobbies</h6><p>INFO SUPP</p></li>
-                <li><h6>hobbies</h6><p>INFO SUPP</p></li>
+                <li><h6>Travel the world</h6><p>Once a year</p></li>
+                <li><h6>Movies<br>Series</h6><p>Nearly every day</p></li>
+                <li><h6>Drawing<br>Painting</h6><p>Every week</p></li>
+                <li><h6>Renovate my house</h6><p>On any occasion</p></li>
             </ul>
         </div>
     </div>
