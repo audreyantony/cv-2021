4b9d4e2aee19338f41499ed6ec63c7ed6ab8091e
audreyantony <web2020.audrey@gmail.com>
2021-03-02 11:04:55 +0100

    connected viex changes

--- public/index.php
+++ public/index.php
@@ -23,7 +23,7 @@ try{
 $userManager = new UserManager($connection);
 
 // ADMIN CONTROLLER IF SOMEONE IS CONNECTED AND IF THIS PERSON HAS A PERMISSION
-if(isset($_SESSION['session_id'])&&$_SESSION['session_id']==session_id()){
+if(isset($_SESSION['sessionId'])&&$_SESSION['sessionId']==session_id()){
 
     require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'controller' . DIRECTORY_SEPARATOR . 'admin' . DIRECTORY_SEPARATOR . 'main.admin.controller.php';
 
